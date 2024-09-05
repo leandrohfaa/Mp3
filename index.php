bc5de49adf4fba3512bc9269339ede5a5ad91bc0
leandrohfaa <leandrohfa@icloud.com>
2024-09-04 22:29:59 -0300

    up

--- index.php
+++ index.php
@@ -2,21 +2,22 @@
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <title>Mp3 em Php</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
     <div class="container">
         <?php
-            if (!isset($_GET['pages'])) {
-                if (file_exists("pages/{$_GET['pages']}.php")) {
-                include_once "pages/{$_GET['pages']}.php";
+            if (isset($_GET['Mp3/pages'])) {
+                if (file_exists("Mp3/pages/{$_GET['pages']}.php")) {
+                    include_once "Mp3/pages/{$_GET['pages']}.php";
+                } else {
+                    include_once 'Mp3/pages/error404.php';
+                }
             } else {
-                include_once 'pages/error404.php';
+                include_once 'Mp3/pages/albums.php';
             }
-        } else {
-            include_once 'pages/albums.php';
-        }
         ?>
     </div> 
   </body>
--- pages/albums.php
+++ pages/albums.php
@@ -1,1 +1,16 @@
-albums
+<h1>Álbum</h1>
+
+<div class="row">
+    <?php
+        for ($i=1; $i <=10; $i++):
+    ?>
+    <div class="col-3 album">
+        <a href="">
+            <img src="/Mp3/albums/ben.jpg" alt="<?$i?>" class="img-albums">
+            <h4>Álbum <?$i?></h4>
+        </a>
+    </div>
+    <?php
+        endfor;
+    ?>
+</div>
